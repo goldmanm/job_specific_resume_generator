ed2f82966aef295caebc4f2c01389c800ee0f045
MarkGoldman <goldmanm@mit.edu>
2016-07-25 23:59:08 -0400

    added functional test for yaml processing

--- functional_tests.py
+++ functional_tests.py
@@ -26,8 +26,45 @@ class functional_test(unittest.TestCase):
          import resume_preferences as pref
     
     def test_convert_yaml_to_python_objects(self):
-        pass
+        # Jun inputs her resume
+        yaml_resume = """
+        header:
+                name : Jun McResume
+                address : 30 Pleasent St.
+    		city : Suburbian
+    		state: Texas
+    		zipcode : 12345
+    		phone : '+1 (XXX)XXX-XXXX'
+			email : 'cliche@aol.net'
+    	
+    	- topic : Education
+    	  year  : 2017
+    	  tasks : 
+    		- name : Ph.D. Engineering
+    		  time : 2010-2012
+    		  org  : Massachusetts Institute of Technology
+    		  points:
+    		  	- text: advanced knowledge of catalytic systems for biogas recovery
+    			  emphasis: ['education','research']
+    	  points : 
+    	  	- text: I got a good education
+    	  	  emphasis : ['education']
+    	  	  
+    	- topic : Work
+    	"""
+        # Jun runs the resume through the engine
+        generator.run_yaml(yaml_resume, categories = ['Education','Work'], emphasis = ['education','industry'], year = 2014)
         
+        # Jun examines the produced LaTeX file
+        self.fail('finish writing the functional test')
+        
+        # Jun ensures the header is properly formated the latex file
+        
+        # Jun ensures 'Education' appears as a header in the file
+        
+        # Jun ensures the point on ____ does not appear in the file
+        
+        # Jun ensures that category points are rendered effectively
         
     def converts_python_objects_to_latex(self):
         pass
